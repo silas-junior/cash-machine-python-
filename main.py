af7cc1fa4cd07c33ceb3fad5549f6a832661d0cf
Silas Junior <silas.pereirajunior04@outlook.com>
2021-03-15 18:22:43 -0300

    organizing code in functions

--- main.py
+++ main.py
@@ -28,6 +28,9 @@ money_notes = {
 }
 
 
+# selected_options = 0
+
+
 def main():
     auth = False
     while not auth:
@@ -36,94 +39,96 @@ def main():
         account = login()
 
         if account:
+            clear_terminal()
             auth = True
-            selected_options = 0
-            show_last_option = str
+            message = str
             admin = accounts_list[account]['admin']
-
-            clear()
-
             name = accounts_list[account]['name']
             balance = accounts_list[account]['balance']
+
             header(name)
 
             while auth:
                 option_typed = menu_options(account)
 
-                if option_typed == '1':
-                    show_last_option = f"Seu saldo: R$ {balance},00"
-                    selected_options += 1
-                elif option_typed == '2':
-                    value_typed = input('Digite o valor que você quer sacar: ')
+                exec_option(option_typed, account, balance, admin, auth)
 
-                    money_notes_user = {}
-                    value = int(value_typed)
+                # if option_typed == '1':
+                #     show_balance()
+                # elif option_typed == '2':
+                #     value_typed = input('Digite o valor que você quer sacar: ')
+                #
+                #     money_notes_user = {}
+                #     value = int(value_typed)
+                #
+                #     rest = balance - int(value_typed)
+                #
+                #     if 0 < value // 100 <= money_notes['100']:
+                #         money_notes_user['100'] = value // 100
+                #         value = value - value // 100 * 100
+                #
+                #     if 0 < value // 50 <= money_notes['50']:
+                #         money_notes_user['50'] = value // 50
+                #         value = value - value // 50 * 50
+                #
+                #     if 0 < value // 20 <= money_notes['20']:
+                #         money_notes_user['20'] = value // 20
+                #         value = value - value // 20 * 20
+                #
+                #     if value != 0:
+                #         total = 0
+                #         for key, value in money_notes.items():
+                #             multi = int(key) * value
+                #             total += multi
+                #
+                #         message = "Não foi possível realizar o saque. \nO caixa não tem cédulas disponíveis para este valor."
+                #
+                #         selected_options += 1
+                #     elif value == 0 and balance > int(value_typed):
+                #
+                #         message = f"Saldo: R$ {balance},00 \nVocê está sacando: R$ {value_typed},00 \nNa sua conta irá sobrar: R$ {rest},00 \nAguarde até que as notas sejam liberadas."
+                #         for note in money_notes_user:
+                #             money_notes[note] -= money_notes_user[note]
+                #         selected_options += 1
+                #
+                #     elif balance < int(value_typed):
+                #         message = f'Valor de saque maior que o valor em conta!\nVocê quer sacar R$ {value_typed},00 mas possui R$ {balance},00 em sua conta'
+                #         selected_options += 1
+                #
+                # elif option_typed == '3':
+                #     auth = False
+                #     message = "Até logo!"
+                # elif option_typed == '4' and admin:
+                #     amount = input('Digite a quantidade de cédulas: ')
+                #     note = input('Digite a cédula a ser inclúida: ')
+                #     selected_options += 1
+                #
+                #     if note not in money_notes:
+                #         print('Vocẽ quer inserir uma nota que não existe')
+                #         selected_options += 1
+                #     else:
+                #         money_notes[note] += int(amount)
+                #         message = f"Você inseriu {amount} notas de {note} reais"
+                #         print(money_notes)
+                #         selected_options += 1
 
-                    rest = balance - int(value_typed)
+                # else:
+                #     message = "A opção que você escolheu não existe"
+                #     selected_options += 1
 
-                    if 0 < value // 100 <= money_notes['100']:
-                        money_notes_user['100'] = value // 100
-                        value = value - value // 100 * 100
-
-                    if 0 < value // 50 <= money_notes['50']:
-                        money_notes_user['50'] = value // 50
-                        value = value - value // 50 * 50
-
-                    if 0 < value // 20 <= money_notes['20']:
-                        money_notes_user['20'] = value // 20
-                        value = value - value // 20 * 20
-
-                    if value != 0:
-                        total = 0
-                        for key, value in money_notes.items():
-                            multi = int(key) * value
-                            total += multi
-
-                        show_last_option = "Não foi possível realizar o saque. \nO caixa não tem cédulas disponíveis para este valor."
-
-                        selected_options += 1
-                    elif value == 0 and balance > int(value_typed):
-
-                        show_last_option = f"Saldo: R$ {balance},00 \nVocê está sacando: R$ {value_typed},00 \nNa sua conta irá sobrar: R$ {rest},00 \nAguarde até que as notas sejam liberadas."
-                        selected_options += 1
-
-                    elif balance < int(value_typed):
-                        show_last_option = f'Valor de saque maior que o valor em conta!\nVocê quer sacar R$ {value_typed},00 mas possui R$ {balance},00 em sua conta'
-                        selected_options += 1
-
-                elif option_typed == '3':
-                    auth = False
-                    show_last_option = "Até logo!"
-                elif option_typed == '4' and admin:
-                    amount = input('Digite a quantidade de cédulas: ')
-                    note = input('Digite a cédula a ser inclúida: ')
-                    selected_options += 1
-
-                    if note not in money_notes:
-                        print('Vocẽ quer inserir uma nota que não existe')
-                        selected_options += 1
-                    else:
-                        money_notes[note] += int(amount)
-                        show_last_option = f"Você inseriu {amount} notas de {note} reais"
-                        print(money_notes)
-                        selected_options += 1
-
-                else:
-                    show_last_option = "A opção que você escolheu não existe"
-                    selected_options += 1
-
-                if selected_options >= 1:
-                    clear()
-                    show_option(show_last_option)
-                    selected_options = 0
+                # selected_options = increment_options()
+                # if selected_options >= 1:
+                #     clear()
+                #     show_message(message)
+                # selected_options = 0
 
         else:
-            show_last_option = "Acesso negado. Verifique conta e senha"
+            clear_terminal()
+            message = "Acesso negado. Verifique conta e senha"
+            show_message(message)
 
-        # input("Pressione <ENTER> para continuar...")
-
-        clear()
-        show_option(show_last_option)
+        clear_terminal()
+        show_message(message)
 
 
 def header(name='default'):
@@ -135,6 +140,91 @@ def header(name='default'):
         print("    Seja bem-vindo, " + name)
         print("****************************************")
         print("****************************************")
+
+
+def increment_options():
+    selected_options = 0
+    return selected_options + 1
+
+
+def exec_option(option_typed, account, balance, admin, auth):
+    if option_typed == '1':
+        clear_terminal()
+        message = f"Seu saldo: R$ {balance},00"
+        show_message(message)
+
+    elif option_typed == '2':
+        withdrawal(balance)
+
+    elif option_typed == '3':
+        clear_terminal()
+        message = "Até logo!"
+        show_message(message)
+
+    elif option_typed == '4' and admin:
+        include_notes()
+        # selected_options += 1
+    else:
+        clear_terminal()
+        message = "A opção que você escolheu não existe!"
+        show_message(message)
+
+
+def show_balance(balance):
+    message = f"Seu saldo: R$ {balance},00"
+    show_message(message)
+
+
+def withdrawal(balance):
+    value_typed = input('Digite o valor que você quer sacar: ')
+
+    money_notes_user = {}
+    value = int(value_typed)
+
+    rest = balance - int(value_typed)
+
+    if 0 < value // 100 <= money_notes['100']:
+        money_notes_user['100'] = value // 100
+        value = value - value // 100 * 100
+
+    if 0 < value // 50 <= money_notes['50']:
+        money_notes_user['50'] = value // 50
+        value = value - value // 50 * 50
+
+    if 0 < value // 20 <= money_notes['20']:
+        money_notes_user['20'] = value // 20
+        value = value - value // 20 * 20
+
+    if value != 0:
+        total = 0
+        for key, value in money_notes.items():
+            multi = int(key) * value
+            total += multi
+
+        clear_terminal()
+        message = "Não foi possível realizar o saque. \nO caixa não tem cédulas disponíveis para este valor."
+        show_message(message)
+
+    elif value == 0 and balance > int(value_typed):
+        clear_terminal()
+        message = f"Saldo: R$ {balance},00 \nVocê está sacando: R$ {value_typed},00 \nNa sua conta irá sobrar: R$ {rest},00 \nAguarde até que as notas sejam liberadas."
+        for note in money_notes_user:
+            money_notes[note] -= money_notes_user[note]
+        show_message(message)
+
+    elif balance < int(value_typed):
+        clear_terminal()
+        message = f'Valor de saque maior que o valor em conta!\nVocê quer sacar R$ {value_typed},00 mas possui R$ {balance},00 em sua conta'
+        show_message(message)
+
+
+def logout():
+    auth = False
+    return auth
+
+
+def include_notes():
+    pass
 
 
 def login():
@@ -154,17 +244,18 @@ def menu_options(account):
 
     if accounts_list[account]['admin']:
         print("4 - Incluir cédulas")
+
     return input("Escolha uma das opções acima: ")
 
 
-def clear():
+def clear_terminal():
     clear = 'cls' if os.name == 'nt' else 'clear'
     os.system(clear)
 
 
-def show_option(option_content):
+def show_message(message):
     print("****************************************")
-    print(option_content)
+    print(message)
     print("****************************************")
 
 
